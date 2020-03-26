51c0c478fa97ad43d4a01ea93526d257130caba2
masouduut94 <masouduut94@gmail.com>
2020-03-26 13:13:11 +0430

    # Video file process completed in `face_detection_server.py`

---
+++ face_detection_server.py
@@ -0,0 +1,244 @@
+from dotenv import load_dotenv
+import argparse
+from os import getenv, makedirs
+from os.path import splitext, basename, exists, isfile
+import numpy as np
+
+from utils.nms.py_cpu_nms import py_cpu_nms
+from utils.tools import *
+from utils.box_utils import decode, decode_landm
+from data import cfg_mnet, cfg_re50
+from layers.functions.prior_box import PriorBox
+from tqdm import tqdm
+
+load_dotenv('.env')
+
+
+def arg_parse():
+    parser = argparse.ArgumentParser(description='Retinaface')
+    parser.add_argument('-m', '--trained_model',
+                        default=getenv('resnet'),
+                        type=str,
+                        help='Trained state_dict file path to open')
+
+    parser.add_argument('--network',
+                        default='resnet50',
+                        help='Backbone network mobile0.25 or resnet50')
+
+    parser.add_argument('--cpu',
+                        action="store_true",
+                        default=False,
+                        help='Use cpu inference')
+
+    parser.add_argument('--confidence_threshold',
+                        default=getenv('confidence_threshold'),
+                        type=float,
+                        help='confidence_threshold')
+
+    parser.add_argument('--top_k',
+                        default=getenv('top_k'),
+                        type=int,
+                        help='top_k')
+
+    parser.add_argument('--nms_threshold',
+                        default=getenv('nms_threshold'),
+                        type=float,
+                        help='nms_threshold')
+
+    parser.add_argument('--step',
+                        default=2,
+                        type=int,
+                        help='speed up the process')
+
+    parser.add_argument('--keep_top_k',
+                        default=getenv('keep_top_k'),
+                        type=int,
+                        help='keep_top_k')
+
+    parser.add_argument('--vis_thres',
+                        default=getenv('vis_thres'),
+                        type=float,
+                        help='visualization_threshold')
+
+    parser.add_argument('--input',
+                        default='upload/test.mp4',
+                        type=str,
+                        help='video input for process')
+
+    parser.add_argument('--output-dir',
+                        default=getenv('output_dir'))
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = arg_parse()
+
+    filename, extension = splitext(basename(args.input))
+    print("Loading file [{}] ....".format(filename))
+
+    if not exists(args.input):
+        raise ValueError("File [{}] is not recognized".format(args.input))
+
+    if not isfile(args.trained_model):
+        raise ValueError(f'The model {args.trained_model} is not found')
+
+    if not exists(args.output_dir):
+        print(f'Output directory {args.output_dir} does not exist, Creating one')
+        makedirs(args.output_dir)
+
+    torch.set_grad_enabled(False)
+    cfg = cfg_mnet if args.network == "mobile0.25" else cfg_re50
+    resize = 1
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    net = model_cfg(args.trained_model, cfg=cfg, device=device, cpu=args.cpu)
+
+    if is_video(extension):
+        vdo = cv2.VideoCapture()
+        codec = cv2.VideoWriter_fourcc(*'XVID')
+        output = args.output_dir + filename + '.avi'
+
+        if vdo.open(args.input):
+            property_id = int(cv2.CAP_PROP_FRAME_COUNT)
+            total_frames = int(cv2.VideoCapture.get(vdo, property_id))
+            width = int(vdo.get(cv2.CAP_PROP_FRAME_WIDTH))
+            height = int(vdo.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            fps = vdo.get(cv2.CAP_PROP_FPS)
+            writer = cv2.VideoWriter(output, codec, fps, (width, height))
+
+            print('')
+            print('processing video ...')
+            with tqdm(total=total_frames) as pbar:
+                while vdo.grab():
+                    pbar.update(1)
+                    ret, frame = vdo.retrieve()
+
+                    if not ret:
+                        break
+
+                    img = np.float32(frame)
+                    im_height, im_width, _ = img.shape
+                    scale = torch.Tensor([img.shape[1], img.shape[0], img.shape[1], img.shape[0]])
+                    img -= (104, 117, 123)
+                    img = img.transpose(2, 0, 1)
+                    img = torch.from_numpy(img).unsqueeze(0)
+                    img = img.to(device)
+                    scale = scale.to(device)
+
+                    loc, conf, landms = net(img)  # forward pass
+                    priorbox = PriorBox(cfg, image_size=(im_height, im_width))
+                    priors = priorbox.forward()
+                    priors = priors.to(device)
+                    prior_data = priors.data
+                    boxes = decode(loc.data.squeeze(0), prior_data, cfg['variance'])
+                    boxes = boxes * scale / resize
+                    boxes = boxes.cpu().numpy()
+                    scores = conf.squeeze(0).data.cpu().numpy()[:, 1]
+                    landms = decode_landm(landms.data.squeeze(0), prior_data, cfg['variance'])
+                    scale1 = torch.Tensor([img.shape[3], img.shape[2], img.shape[3], img.shape[2],
+                                           img.shape[3], img.shape[2], img.shape[3], img.shape[2],
+                                           img.shape[3], img.shape[2]])
+                    scale1 = scale1.to(device)
+                    landms = landms * scale1 / resize
+                    landms = landms.cpu().numpy()
+
+                    # ignore low scores
+                    inds = np.where(scores > args.confidence_threshold)[0]
+                    boxes = boxes[inds]
+                    landms = landms[inds]
+                    scores = scores[inds]
+
+                    # keep top-K before NMS
+                    order = scores.argsort()[::-1][:args.top_k]
+                    boxes = boxes[order]
+                    landms = landms[order]
+                    scores = scores[order]
+
+                    # do NMS
+                    dets = np.hstack((boxes, scores[:, np.newaxis])).astype(np.float32, copy=False)
+                    keep = py_cpu_nms(dets, args.nms_threshold)
+                    dets = dets[keep, :]
+                    landms = landms[keep]
+
+                    # keep top-K faster NMS
+                    dets = dets[:args.keep_top_k, :]
+                    landms = landms[:args.keep_top_k, :]
+                    dets = np.concatenate((dets, landms), axis=1)
+
+                    objects_to_draw = dict(draw_box=True, draw_text=True, draw_landmarks=True)
+                    frame = draw(frame, dets, args.vis_thres, **objects_to_draw)
+
+                    writer.write(frame)
+
+                print('process finished Successfully')
+                print('process finished. file is stored as {}'.format(output))
+
+                pbar.close()
+                vdo.release()
+                writer.release()
+
+    elif is_image(extension):
+        frame = cv2.imread(args.input)
+        img = np.float32(frame)
+        im_height, im_width, _ = img.shape
+        scale = torch.Tensor([img.shape[1], img.shape[0], img.shape[1], img.shape[0]])
+        img -= (104, 117, 123)
+        img = img.transpose(2, 0, 1)
+        img = torch.from_numpy(img).unsqueeze(0)
+        img = img.to(device)
+        scale = scale.to(device)
+
+        loc, conf, landms = net(img)  # forward pass
+        priorbox = PriorBox(cfg, image_size=(im_height, im_width))
+        priors = priorbox.forward()
+        priors = priors.to(device)
+        prior_data = priors.data
+        boxes = decode(loc.data.squeeze(0), prior_data, cfg['variance'])
+        boxes = boxes * scale / resize
+        boxes = boxes.cpu().numpy()
+        scores = conf.squeeze(0).data.cpu().numpy()[:, 1]
+        landms = decode_landm(landms.data.squeeze(0), prior_data, cfg['variance'])
+        scale1 = torch.Tensor([img.shape[3], img.shape[2], img.shape[3], img.shape[2],
+                               img.shape[3], img.shape[2], img.shape[3], img.shape[2],
+                               img.shape[3], img.shape[2]])
+        scale1 = scale1.to(device)
+        landms = landms * scale1 / resize
+        landms = landms.cpu().numpy()
+
+        # ignore low scores
+        inds = np.where(scores > args.confidence_threshold)[0]
+        boxes = boxes[inds]
+        landms = landms[inds]
+        scores = scores[inds]
+
+        # keep top-K before NMS
+        order = scores.argsort()[::-1][:args.top_k]
+        boxes = boxes[order]
+        landms = landms[order]
+        scores = scores[order]
+
+        # do NMS
+        dets = np.hstack((boxes, scores[:, np.newaxis])).astype(np.float32, copy=False)
+        keep = py_cpu_nms(dets, args.nms_threshold)
+        dets = dets[keep, :]
+        landms = landms[keep]
+
+        # keep top-K faster NMS
+        dets = dets[:args.keep_top_k, :]
+        landms = landms[:args.keep_top_k, :]
+        dets = np.concatenate((dets, landms), axis=1)
+
+        objects_to_draw = dict(draw_box=True, draw_text=False, draw_landmarks=False)
+        frame = draw(frame, dets, args.vis_thres, **objects_to_draw)
+
+        output = args.output_dir + filename + '.jpg'
+        cv2.imwrite(output, frame)
+        print('output is stored as {}'.format(output))
+
+    else:
+        print('cant read video {}'.format(args.input))
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils/tools.py
@@ -0,0 +1,147 @@
+import torch
+from models.retinaface import RetinaFace
+import torch.backends.cudnn as cudnn
+import cv2
+
+
+def is_video(ext: str):
+    exts = (
+        '.264', '.3g2', '.3gp', '.3gp2', '.3gpp', '.3gpp2', '.3mm', '.3p2', '.60d', '.787', '.89', '.aaf', '.aec',
+        '.aep', '.aepx',
+        '.aet', '.aetx', '.ajp', '.ale', '.am', '.amc', '.amv', '.amx', '.anim', '.aqt', '.arcut', '.arf', '.asf',
+        '.asx', '.avb',
+        '.avc', '.avd', '.avi', '.avp', '.avs', '.avs', '.avv', '.axm', '.bdm', '.bdmv', '.bdt2', '.bdt3', '.bik',
+        '.bin', '.bix',
+        '.bmk', '.bnp', '.box', '.bs4', '.bsf', '.bvr', '.byu', '.camproj', '.camrec', '.camv', '.ced', '.cel', '.cine',
+        '.cip',
+        '.clpi', '.cmmp', '.cmmtpl', '.cmproj', '.cmrec', '.cpi', '.cst', '.cvc', '.cx3', '.d2v', '.d3v', '.dat',
+        '.dav', '.dce',
+        '.dck', '.dcr', '.dcr', '.ddat', '.dif', '.dir', '.divx', '.dlx', '.dmb', '.dmsd', '.dmsd3d', '.dmsm',
+        '.dmsm3d', '.dmss',
+        '.dmx', '.dnc', '.dpa', '.dpg', '.dream', '.dsy', '.dv', '.dv-avi', '.dv4', '.dvdmedia', '.dvr', '.dvr-ms',
+        '.dvx', '.dxr',
+        '.dzm', '.dzp', '.dzt', '.edl', '.evo', '.eye', '.ezt', '.f4p', '.f4v', '.fbr', '.fbr', '.fbz', '.fcp',
+        '.fcproject',
+        '.ffd', '.flc', '.flh', '.fli', '.flv', '.flx', '.gfp', '.gl', '.gom', '.grasp', '.gts', '.gvi', '.gvp',
+        '.h264', '.hdmov',
+        '.hkm', '.ifo', '.imovieproj', '.imovieproject', '.ircp', '.irf', '.ism', '.ismc', '.ismv', '.iva', '.ivf',
+        '.ivr', '.ivs',
+        '.izz', '.izzy', '.jss', '.jts', '.jtv', '.k3g', '.kmv', '.ktn', '.lrec', '.lsf', '.lsx', '.m15', '.m1pg',
+        '.m1v', '.m21',
+        '.m21', '.m2a', '.m2p', '.m2t', '.m2ts', '.m2v', '.m4e', '.m4u', '.m4v', '.m75', '.mani', '.meta', '.mgv',
+        '.mj2', '.mjp',
+        '.mjpg', '.mk3d', '.mkv', '.mmv', '.mnv', '.mob', '.mod', '.modd', '.moff', '.moi', '.moov', '.mov', '.movie',
+        '.mp21',
+        '.mp21', '.mp2v', '.mp4', '.mp4v', '.mpe', '.mpeg', '.mpeg1', '.mpeg4', '.mpf', '.mpg', '.mpg2', '.mpgindex',
+        '.mpl',
+        '.mpl', '.mpls', '.mpsub', '.mpv', '.mpv2', '.mqv', '.msdvd', '.mse', '.msh', '.mswmm', '.mts', '.mtv', '.mvb',
+        '.mvc',
+        '.mvd', '.mve', '.mvex', '.mvp', '.mvp', '.mvy', '.mxf', '.mxv', '.mys', '.ncor', '.nsv', '.nut', '.nuv',
+        '.nvc', '.ogm',
+        '.ogv', '.ogx', '.osp', '.otrkey', '.pac', '.par', '.pds', '.pgi', '.photoshow', '.piv', '.pjs', '.playlist',
+        '.plproj',
+        '.pmf', '.pmv', '.pns', '.ppj', '.prel', '.pro', '.prproj', '.prtl', '.psb', '.psh', '.pssd', '.pva', '.pvr',
+        '.pxv',
+        '.qt', '.qtch', '.qtindex', '.qtl', '.qtm', '.qtz', '.r3d', '.rcd', '.rcproject', '.rdb', '.rec', '.rm', '.rmd',
+        '.rmd',
+        '.rmp', '.rms', '.rmv', '.rmvb', '.roq', '.rp', '.rsx', '.rts', '.rts', '.rum', '.rv', '.rvid', '.rvl', '.sbk',
+        '.sbt',
+        '.scc', '.scm', '.scm', '.scn', '.screenflow', '.sec', '.sedprj', '.seq', '.sfd', '.sfvidcap', '.siv', '.smi',
+        '.smi',
+        '.smil', '.smk', '.sml', '.smv', '.spl', '.sqz', '.srt', '.ssf', '.ssm', '.stl', '.str', '.stx', '.svi', '.swf',
+        '.swi',
+        '.swt', '.tda3mt', '.tdx', '.thp', '.tivo', '.tix', '.tod', '.tp', '.tp0', '.tpd', '.tpr', '.trp', '.ts',
+        '.tsp', '.ttxt',
+        '.tvs', '.usf', '.usm', '.vc1', '.vcpf', '.vcr', '.vcv', '.vdo', '.vdr', '.vdx', '.veg', '.vem', '.vep', '.vf',
+        '.vft',
+        '.vfw', '.vfz', '.vgz', '.vid', '.video', '.viewlet', '.viv', '.vivo', '.vlab', '.vob', '.vp3', '.vp6', '.vp7',
+        '.vpj',
+        '.vro', '.vs4', '.vse', '.vsp', '.w32', '.wcp', '.webm', '.wlmp', '.wm', '.wmd', '.wmmp', '.wmv', '.wmx',
+        '.wot', '.wp3',
+        '.wpl', '.wtv', '.wve', '.wvx', '.xej', '.xel', '.xesc', '.xfl', '.xlmv', '.xmv', '.xvid', '.y4m', '.yog',
+        '.yuv', '.zeg',
+        '.zm1', '.zm2', '.zm3', '.zmv'
+    )
+    vid = any((ext == x for x in exts))
+    return vid
+
+
+def is_image(ext: str):
+    exts = ('.png', '.jpg', '.jpeg',
+            '.bmp', '.pbm', '.pgm',
+            '.ppm', '.jpe', '.tiff',
+            '.tif')
+
+    return any((ext == x for x in exts))
+
+
+def model_cfg(trained_model, cfg, device, cpu):
+    net = RetinaFace(cfg=cfg, phase='test')
+    net = load_model(net, trained_model, load_to_cpu=cpu)
+    net.eval()
+    print('Finished loading model!')
+    cudnn.benchmark = True
+    net = net.to(device)
+    return net
+
+
+def check_keys(model, pretrained_state_dict):
+    ckpt_keys = set(pretrained_state_dict.keys())
+    model_keys = set(model.state_dict().keys())
+    used_pretrained_keys = model_keys & ckpt_keys
+    unused_pretrained_keys = ckpt_keys - model_keys
+    missing_keys = model_keys - ckpt_keys
+    print('Missing keys:{}'.format(len(missing_keys)))
+    print('Unused checkpoint keys:{}'.format(len(unused_pretrained_keys)))
+    print('Used keys:{}'.format(len(used_pretrained_keys)))
+    assert len(used_pretrained_keys) > 0, 'load NONE from pretrained checkpoint'
+    return True
+
+
+def remove_prefix(state_dict, prefix):
+    ''' Old style model is stored with all names of parameters sharing common prefix 'module.' '''
+    print('remove prefix \'{}\''.format(prefix))
+    f = lambda x: x.split(prefix, 1)[-1] if x.startswith(prefix) else x
+    return {f(key): value for key, value in state_dict.items()}
+
+
+def load_model(model, pretrained_path, load_to_cpu):
+    print('Loading pretrained model from {}'.format(pretrained_path))
+    if load_to_cpu:
+        pretrained_dict = torch.load(pretrained_path, map_location=lambda storage, loc: storage)
+    else:
+        device = torch.cuda.current_device()
+        pretrained_dict = torch.load(pretrained_path, map_location=lambda storage, loc: storage.cuda(device))
+    if "state_dict" in pretrained_dict.keys():
+        pretrained_dict = remove_prefix(pretrained_dict['state_dict'], 'module.')
+    else:
+        pretrained_dict = remove_prefix(pretrained_dict, 'module.')
+    check_keys(model, pretrained_dict)
+    model.load_state_dict(pretrained_dict, strict=False)
+    return model
+
+
+def draw(frame, dets, visual_threshold, draw_box=True, draw_text=False, draw_landmarks=False):
+    for b in dets:
+        if b[4] < visual_threshold:
+            continue
+
+        text = "{:.2f}".format(b[4])
+        b = list(map(int, b))
+        if draw_box:
+            cv2.rectangle(frame, (b[0], b[1]), (b[2], b[3]), (0, 0, 255), 2)
+        if draw_text:
+            cx = b[0]
+            cy = b[1] + 12
+            cv2.putText(frame, text, (cx, cy),
+                        cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255))
+
+        # landmarks
+        if draw_landmarks:
+            cv2.circle(frame, (b[5], b[6]), 1, (0, 0, 255), 4)
+            cv2.circle(frame, (b[7], b[8]), 1, (0, 255, 255), 4)
+            cv2.circle(frame, (b[9], b[10]), 1, (255, 0, 255), 4)
+            cv2.circle(frame, (b[11], b[12]), 1, (0, 255, 0), 4)
+            cv2.circle(frame, (b[13], b[14]), 1, (255, 0, 0), 4)
+
+    return frame
